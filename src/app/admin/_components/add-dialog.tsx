c6a97cb7881dc48c313c44a4c0351807ca68d538
ehab <ftwehab@gmail.com>
2025-04-24 23:07:25 +0200

    update import type for add diploma

--- src/app/admin/_components/add-dialog.tsx
+++ src/app/admin/_components/add-dialog.tsx
@@ -5,31 +5,32 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { TAddDiplomDialog } from "@/lib/types/components-props";
+import { TAddDiplomaDialog } from "@/lib/types/components-props";
+
 import { IoArrowBackCircleOutline } from "react-icons/io5";
 
 export default function AddDiplomaDialog({
   children,
   ...addDiplomaDialogProps
-}: TAddDiplomDialog) {
+}: TAddDiplomaDialog) {
   // Destruct props
 
   const { title, open, setOpen, action, error } = addDiplomaDialogProps;
 
-  function dialogControle() {
+  function dialogControl() {
     setOpen(!open);
   }
 
   return (
     <>
       <Dialog open={open}>
-        <DialogTrigger onClick={dialogControle}>{children}</DialogTrigger>
+        <DialogTrigger onClick={dialogControl}>{children}</DialogTrigger>
         <DialogContent className="flex w-[600px] flex-col">
           {error && <p className="text-red-500">{error}</p>}
           <DialogTitle className="flex items-center gap-2 text-2xl font-bold text-primary">
             <IoArrowBackCircleOutline
               className="size-6"
-              onClick={dialogControle}
+              onClick={dialogControl}
             />
             <span>{title}</span>
           </DialogTitle>
